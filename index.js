3952510318038d698832eb2118d89149c574df63
Twistehedi <ataul1708@gmail.com>
2022-04-17 11:05:47 -0700

    mehedi

--- index.js
+++ index.js
@@ -4,20 +4,32 @@
 // create server
 //server running
 // handle server
-const http =require('http');
-
+const http = require('http');
+const url =require('url');
+// const url =require('url');
 const app={};
 
 app.config={
-    port:8080
+    port:3000
 };
+
 app.createServer=()=>{
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, ()=>{
-        console.log(`Your server run port number in ${app.config.port}`);
+    server.listen(app.config.port,()=>{
+        console.log(`You can start your programming ${app.config.port}`)
     })
 }
+
 app.handleReqRes=(req, res)=>{
-    res.end('Your work in good')
+ 
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname; 
+    // console.log(path)
+
+    const trimmedPath = path.replace(,'');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headerObject = req.headers;
+    res.end('Hello programmer you are great programmer');
 }
 app.createServer();
